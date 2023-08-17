b0d38da27e91cba3ff1fcb5090a32948a2b6bd1c
Tom <1029520042@qq.com>
2023-08-17 18:56:08 +0800

    build: 引入unplugin-element-plus，以自动引用相应样式

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import {resolve} from 'path'
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import ElementPlus from "unplugin-element-plus/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      "@":resolve(__dirname,"src")
-    }
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
   },
-  plugins: [vue()],
+  plugins: [vue(), ElementPlus({})],
 });
