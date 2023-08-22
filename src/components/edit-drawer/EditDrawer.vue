cb751a1d24297ac242b4415505c07859d530da59
Tom <1029520042@qq.com>
2023-08-23 00:48:50 +0800

    feat: 完成 EditDrawer 组件的展开/缩起功能

---
+++ src/components/edit-drawer/EditDrawer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="$style.a" :style="isSpread ? 'transform:translateY(0)' : ''">
+    <SpreadButton
+      :class="$style.b"
+      @click="handleClick"
+      :is-spread="isSpread"
+    />
+    <Tabs></Tabs>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import Tabs from "@/components/edit-tabs/index.vue";
+import SpreadButton from "./SpreadButton.vue";
+
+const isSpread = ref(false);
+function handleClick() {
+  isSpread.value = !isSpread.value;
+}
+</script>
+<style module>
+.a {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  box-shadow: 0 0 20px rgb(0, 0, 0, 0.2);
+  transform: translateY(calc(100% - 55px)); /* 55px 是 TabsLabel的高度 */
+  transition: all 0.3s ease-out 0s;
+}
+.b {
+  position: absolute;
+  left: 50%;
+  top: -35px;
+  z-index: 1;
+  margin-left: -40px;
+  transition: all 0.2s ease-out 0s;
+}
+.b:hover {
+  top: -40px;
+}
+</style>
---
+++ src/components/edit-drawer/SpreadButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :class="$style.a">
+    <el-icon v-if="!isSpread" color="#13d8a7" size="35px"><ArrowUp /></el-icon>
+    <el-icon v-else color="#13d8a7" size="35px"><ArrowDown /></el-icon>
+  </button>
+</template>
+<script lang="ts" setup>
+import { ElIcon } from "element-plus";
+import { ArrowUp, ArrowDown } from "@element-plus/icons-vue";
+defineProps({
+  isSpread: Boolean,
+});
+</script>
+<style module>
+.a {
+  width: 80px;
+  height: 80px;
+  border: none;
+  border-radius: 50%;
+  padding-bottom: 30px;
+  background-color: #fff;
+  cursor: pointer;
+  box-shadow: 0 0 20px rgb(0, 0, 0, 0.2);
+}
+
+</style>
