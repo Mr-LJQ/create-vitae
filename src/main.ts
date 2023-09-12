80731b74683bdc6a2e14c9259f344bea24106ba5
Tom <1029520042@qq.com>
2023-09-12 16:47:43 +0800

    feat: 添加数据本地缓存能力

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createPersistedStatePlugin } from "pinia-plugin-persistedstate-2";
+import localforage from "localforage";
 import App from "./App.vue";
 import "normalize.css";
 import "./index.css";
 import "@/styles/index.scss";
 const pinia = createPinia();
+pinia.use(
+  createPersistedStatePlugin({
+    storage: {
+      getItem: localforage.getItem.bind(localforage),
+      setItem: localforage.setItem.bind(localforage),
+      removeItem: localforage.removeItem.bind(localforage),
+    },
+  })
+);
 const app = createApp(App);
 app.use(pinia);
 app.mount("#app");
