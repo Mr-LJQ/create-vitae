5a3e97d4fb632f6f666d6db9910eb5e18c307843
Tom <1029520042@qq.com>
2023-09-21 18:26:31 +0800

    chore: 开始 ConfigHeader 组件实现

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="bg-[#39394d] pt-8" :class="OVERRIDE_ELEMENT_PLUS">
-    <el-config-provider :locale="zhCn">
-      <component :is="ATemplate"></component>
+    <ElConfigProvider :locale="zhCn">
+      <ConfigHeader v-slot="{ templateRefCallback }">
+        <!-- ref 的回调函数写法，该语法是正确的，但 ts 错误的的报错，该报错暂时没有更优雅的处理方法  -->
+        <div class="vitae" :ref="templateRefCallback">
+          <component :is="ATemplate"></component>
+        </div>
+      </ConfigHeader>
       <EditDrawer />
-    </el-config-provider>
+    </ElConfigProvider>
   </div>
 </template>
 <script lang="ts" setup>
+import ConfigHeader from "@/features/config-header/index.vue";
 import EditDrawer from "@/features/editor/index.vue";
 import ATemplate from "@/features/templates/a-template/index.vue";
 import { OVERRIDE_ELEMENT_PLUS } from "@/styles";
