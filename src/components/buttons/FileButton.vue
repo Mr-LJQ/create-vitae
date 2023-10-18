27d1a46606ac3f1854a97db17a04dd654c5e1a68
Tom <1029520042@qq.com>
2023-10-18 14:18:41 +0800

    feat: 实现 PictureCropper 按需加载

--- src/components/buttons/FileButton.vue
+++ src/components/buttons/FileButton.vue
@@ -11,12 +11,13 @@
   <PictureCropper
     v-model="isOpen"
     :imageData="imageData"
+    v-if="openPictureCropper"
     @imageChange="handleImageChange"
   />
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
-import { PictureCropper } from "@/components";
+import { ref, defineAsyncComponent, watch } from "vue";
+import { ElNotification } from "element-plus";
 import { BUTTON_FILE_CLASS } from "@/styles";
 defineOptions({
   name: "FileButton",
@@ -25,9 +26,27 @@ defineOptions({
 const emit = defineEmits({
   change: (data: Blob) => data instanceof Blob,
 });
+
+const affirmError = ref(false);
+const PictureCropper = defineAsyncComponent({
+  loader: () => import("@/components/picture-cropper/index.vue"),
+  onError(error) {
+    ElNotification.error({
+      title: "图片裁切相关功能加载失败",
+      message: `错误信息：${error}`,
+      duration: 0,
+    });
+    //处理回退相关逻辑
+    affirmError.value = true;
+  },
+});
+
+const openPictureCropper = ref(false);
 const fileInput = ref<HTMLInputElement>();
 function handleUploadFileClick() {
   fileInput.value?.click();
+  //当用户点击按钮上传图片时，才异步加载图片裁切相关代码
+  openPictureCropper.value = true;
 }
 
 const isOpen = ref(false);
@@ -40,7 +59,35 @@ function handleChange(event: Event) {
   isOpen.value = true;
   //该行的作用是：允许重复上传同一张图片
   input.value = "";
+  /**
+   * 图片裁切功能加载失败，如果想要回退，则需要考虑时序问题
+   *  因为加载失败可能发生在用户上传图片后，也可能发生在用户上传图片前
+   *  也即代码运行到这里，需要考虑多种情况：
+   *    1.失败是已知的（已经发生），此时可以在此运行回退相关代码
+   *    2.失败是未知的：
+   *        - 之后会成功，什么都不用做，现有代码会解决
+   *        - 之后会失败，需要额外的代码处理回退问题（通过下面的 watch 处理该问题）
+   */
+  if (affirmError.value) {
+    handleImageChange(file);
+  }
 }
+
+/**
+ * 此处有两种情况，
+ *  - 文件上传前失败，则因为 mageData.value 的值还为 null，而不执行回退相关逻辑
+ *      而在文件上传后，handleChange 会执行回退相关逻辑
+ *  - 文件上传后失败，则 handleChange 没有执行回退相关逻辑，此时因为 mageData.value 有值，
+ *      所以此处会指向回退相关逻辑
+ * 因为 affirmError 只会发生一次改变，因此此 watch 只会执行一次
+ *
+ */
+const clear = watch(affirmError, () => {
+  if (imageData.value) {
+    handleImageChange(imageData.value);
+  }
+  clear();
+});
 
 function handleImageChange(data: Blob) {
   emit("change", data);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+/**
+ * 此处的目的是为了使得引用更加方便，允许直接使用 "@/components" 这样的短路径进行引用
+ *  但是这可能会导致一个问题，那便是异步加载的代码，因为在这里被引用，因此导致其被同步加载，
+ *  那样的话，异步加载的效果就无法实现。
+ */
 export * from "./tabs";
 export * from "./buttons";
 export * from "./popover";
@@ -5,7 +10,11 @@ export * from "./share-modules";
 import EditInput from "./edit-input/index.vue";
 import EditSwitch from "./edit-switch/index.vue";
 import EditInputItem from "./edit-input-item/index.vue";
-import PictureCropper from "./picture-cropper/index.vue";
+/**
+ * 注意，异步加载的组件不能够在该文件中同步引入，
+ * 否则它会在此处的引用中进行加载，最终导致异步加载无法得到想要的效果
+ */
+//import PictureCropper from "./picture-cropper/index.vue";
 
 import RichTextEditor from "./rich-text-editor/index.vue";
 import ShowCheckedTags from "./show-checked-tags/index.vue";
@@ -16,7 +25,7 @@ export {
   EditSwitch,
   EditInputItem,
   RichTextEditor,
-  PictureCropper,
+  //PictureCropper,
   ShowCheckedTags,
   ReadOnlyRichText,
 };
